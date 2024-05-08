c20e843ba36b4d1fbf4e4c7d35f266ddb961cb61
CristianCastro2023 <kristian19alesana@gmail.com>
2024-05-08 00:08:28 -0300

    cambios_2

--- Front/index.js
+++ Front/index.js
@@ -1,18 +1,17 @@
+import { navBar } from "./components/navBar.js";
+import { homeDisplay } from "./components/homeDisplay.js";
 
-import { navBar } from "../components/navBar.js";
-import { homeDisplay } from "../components/homeDisplay.js";
+const navbarContainer = document.querySelector(".navbar-container");
+const displayDiv = document.querySelector(".display-div");
 
-
-
-
-const CATEGORIES = ['ficción', 'historia', 'recetas', 'cómics'];//add categories here, they'll be dinamically added to the dropdown menu of the navbar
-
-let PAGE_TITLE = 'AUREA';
 const initialize = () => {
-  navBar(CATEGORIES, PAGE_TITLE);
-  homeDisplay();
+  navBar()
+  homeDisplay()
+};
+
+try {
+  initialize();
+} catch (error) {
+  console.error("Error initializing application:", error);
+  // You can add additional error handling logic here, such as displaying an error message to the user
 }
-
-
-
-initialize()//main entry point
